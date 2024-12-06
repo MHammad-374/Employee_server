ca17811cca51eda395a8511741d919c068d72636
MHammad-374 <143274487+MHammad-374@users.noreply.github.com>
2024-12-07 00:51:09 +0500

    Update user_route.js

--- api/routes/user_route.js
+++ api/routes/user_route.js
@@ -2,7 +2,10 @@ const { getUsers, login, deleteUser, updateUser, addUser } = require('../control
 const express = require('express')
 const router = express.Router()
 
-router.get('/', getUsers)
+// router.get('/', getUsers)
+router.get('/', (req,res)=>{
+    res.json({msg: "Hello Users"})
+})
 router.post('/auth/login', login)
 router.delete('/:id', deleteUser)
 router.put('/:id', updateUser)
